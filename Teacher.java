1f36a432474f1493ae723723bc1c62627acf9bc2
ms3254 <mdvshah19@gmail.com>
2025-04-17 20:52:28 +0000

    teacher

--- Teacher.java
+++ Teacher.java
@@ -1,10 +1,14 @@
 public class Teacher extends Person{
     
     // TODO: Add fields.
+    private String subject;
+    private double salary;
 
-    public Teacher(String name, int age, String gender)
+    public Teacher(String name, int age, String gender, String subject, double salary)
     {
         super(name, age, gender);
+        this.subject = subject;
+        this.salary = salary;
     }
 
     // TODO Add methods
@@ -12,5 +16,18 @@ public class Teacher extends Person{
     public String toString(){
         // TODO: improve this.
         return super.toString() + ", is a teacher!";
+    }
+    public String getSubject(){
+        return subject;
+    }
+    
+    public String getSalary(){
+        return salary;
+    }
+    public void setSalary(){
+        salary = 100;
+    }
+    public void set Subject(){
+        subject = "Math";
     }
 }
